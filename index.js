d7ca6853f1005f7ef70c0db7ff46c1f375d70c8e
Lucas Fiorelli <fiorellilucas@gmail.com>
2024-04-14 15:23:10 -0300

    Adicionar mais endpoints para a API

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 import express from "express"
+import { PrismaClient } from "@prisma/client"
 
 const app = express()
 const port = 3000
 
+const prisma = new PrismaClient()
+
 app.get("/", (req, res) => {
     res.send("Hello world")
+})
+
+app.get("/instalacoes", async (req, res) => {
+    let instalacoes = await prisma.instalacao.findMany()
+    res.send(instalacoes)
+})
+
+app.get("/setores", async (req, res) => {
+    let setores = await prisma.setor.findMany()
+    res.send(setores)
+})
+
+app.get("/clientes", async (req, res) => {
+    let clientes = await prisma.cliente.findMany()
+    res.send(clientes)
 })
 
 app.listen(port, () => {
