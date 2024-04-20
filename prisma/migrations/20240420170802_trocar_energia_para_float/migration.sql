23e46d86dce13b4007de0d8268d48ad902bb2126
Lucas Fiorelli <fiorellilucas@gmail.com>
2024-04-20 14:13:13 -0300

    Trocar energia de int para float e remover campo temperatura

---
+++ prisma/migrations/20240420170802_trocar_energia_para_float/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "energiaArmazenada" ALTER COLUMN "energiaArmazenada" SET DATA TYPE DOUBLE PRECISION;
+
+-- AlterTable
+ALTER TABLE "energiaConsumida" ALTER COLUMN "energiaConsumida" SET DATA TYPE DOUBLE PRECISION;
+
+-- AlterTable
+ALTER TABLE "energiaGerada" ALTER COLUMN "energiaGerada" SET DATA TYPE DOUBLE PRECISION;
---
+++ prisma/migrations/20240420170945_retirar_campo_temperatura/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `temperaturaMomento` on the `energiaGerada` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "energiaGerada" DROP COLUMN "temperaturaMomento";
