787afaf3fc4513a715742a645c4e28c493ec4755
Lucas Fiorelli <fiorellilucas@gmail.com>
2024-05-18 23:20:14 -0300

    Criar principais routes do front end

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Root from './routes/root.jsx'
+import { 
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom"
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>
+  },
+  {
+    path: "/dashboard",
+    element: <h1>dashboard</h1>
+  }, 
+  {
+    path: "/dashboard/visao-geral",
+    element: <h1>visão geral</h1>
+  },
+  {
+    path: "/dashboard/estatisticas",
+    element: <h1>estatísticas</h1>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
